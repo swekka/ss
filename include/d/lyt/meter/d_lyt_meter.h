41282562e28cbd348935d2807e3abf870ebc29ef
Sam V. <swekka@proton.me>
2025-12-29 12:56:42 +0100

    Revert "d_lyt_force_line OK"
    This reverts commit 0ecd042e8fdc0298a55ee6069f837227d560cffc.

--- include/d/lyt/meter/d_lyt_meter.h
+++ include/d/lyt/meter/d_lyt_meter.h
@@ -452,14 +452,6 @@ public:
         }
     }
 
-    static bool getfn_800C9F70() {
-        if (sInstance != nullptr) {
-            return sInstance->mMain.fn_800C9F70();
-        } else {
-            return false;
-        }
-    }
-
     static bool getfn_800C9FE0() {
         if (sInstance != nullptr) {
             return sInstance->mMain.fn_800C9FE0();
