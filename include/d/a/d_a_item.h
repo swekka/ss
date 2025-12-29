7e613ddd8252a8210b27889dffa3283cb9b62945
Sam V. <swekka@proton.me>
2025-12-29 12:56:42 +0100

    Revert "d_lyt_mini_game WIP"
    This reverts commit d256390ec6983e3632517ca0022010e912171f8d.

--- include/d/a/d_a_item.h
+++ include/d/a/d_a_item.h
@@ -53,7 +53,6 @@ public:
         return checkFlag(flag);
     }
     static void setFlag(s32 id);
-    static bool checkTreasureTempCollect(u16 itemId);
 
     void setItemPosition(const mVec3_c &);
     void getItemFromBWheelItem();
